6d5dc3a808e00d180cedee4b8bff03f60c373ec0
physicsIsClutch <kfranson@umich.edu>
2016-07-29 11:28:28 -0400

    Created Card class

---
+++ Blackjack/Card.cpp
@@ -0,0 +1,4 @@
+
+#include "Card.h"
+
+
---
+++ Blackjack/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  Blackjack
+//
+//  Created by Kyle Franson on 7/29/16.
+//  Copyright © 2016 Kyle Franson. All rights reserved.
+//
+
+#ifndef Card_h
+#define Card_h
+
+
+#endif /* Card_h */
